ac77e9ddc778cacdbf571461df6c39a211129c7e
evgeni83 <evgeni.daniluk@yandex.ru>
2020-01-29 18:50:25 +0300

    Доработаны точки слайдера, добавлены пропадание точек и появление стрелок при адаптиве

--- js/script.js
+++ js/script.js
@@ -10,6 +10,29 @@ $(document).ready(function () {
       cssEase: 'linear',
       draggable: false,
       swipe: false,
-      zIndex: 0
+      zIndex: 0,
+      responsive: [
+        {
+          breakpoint: 767,
+          settings: {
+            infinite: true,
+            arrows: true,
+            appendArrows: $(".home-page-slider-arrows"),
+            dots: false,
+            swipe: true,
+            draggable: true
+          }
+        }
+      ]
     });
+
+    $('.home-page-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+      console.log(slick);
+      console.log(currentSlide);
+      console.log(nextSlide);
+
+      console.log($(slick.$slides[currentSlide]));
+    });
+
 });
+
