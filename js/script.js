c0a22342ab79e83345c1b6abab314544c7317318
evgeni83 <evgeni.daniluk@yandex.ru>
2020-01-29 16:19:11 +0300

    Сделана главная страница, реализован слайдер

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-    
+    $(".home-page-slider").slick({
+      slidesToShow: 1,
+      arrows: false,
+      dots: true,
+      appendDots: $(".home-page-dots"),
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      draggable: false,
+      swipe: false,
+      zIndex: 0
+    });
 });
