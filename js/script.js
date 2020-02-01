a7aedba16fbc38708743e4213e895727187a1afd
evgeni83 <evgeni.daniluk@yandex.ru>
2020-02-01 21:49:55 +0300

    Исправлена проблема присутствия скролла на браузерах смартфонов

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+window.addEventListener("resize", () => {
+  vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
+
 $(document).ready(function() {
   $(".home-page-slider").css(
     "background-image",
